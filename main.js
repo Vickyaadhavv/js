77d5e3977bdf9fca5ff7c0026e7aadc8c82c38e2
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-06-16 21:03:54 +0530

    bug

--- main.js
+++ main.js
@@ -8,8 +8,18 @@ let counter = 0;
 increment.addEventListener("click",()=>{
     counter++;
     valuecounter.innerHTML= counter;
+    if(counter >=1){
+        valuecounter.style.color= "green" ;  
+    }else if(counter==0){
+        valuecounter.style.color="black" ;
+    }
 });
 decrement.addEventListener("click",()=>{
     counter--;
     valuecounter.innerHTML=counter;
+    if(counter < 1){
+        valuecounter.style.color= "red"   
+    }else if(counter==0){
+        valuecounter.style.color="black" ;
+    }
 });
